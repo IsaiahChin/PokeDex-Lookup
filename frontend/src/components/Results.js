8f5455a1eed19d556b08607e07527a5744e1f822
Isaiah Chin <68924638+isaiah497@users.noreply.github.com>
2021-12-14 01:10:31 +1300

    Added Results component + styling

---
+++ frontend/src/components/Results.js
@@ -0,0 +1,44 @@
+import "./Results.css";
+
+const Results = ({ pokemonData }) => {
+  return pokemonData.map((data, index) => {
+    return (
+      <>
+        <div id="pokemon-profile" key={index}>
+          <h2>{data.name[0].toUpperCase() + data.name.substring(1)}</h2>
+          <div id="pokemon-image">
+            <img src={data.sprites["front_default"]} />
+          </div>
+        </div>
+        <h3>Abilities</h3>
+        <div id="pokemon-abilities">
+          {/* Dynamically display pokemon abilities */}
+          {data.abilities.map((ability) => {
+            let abilityName = ability.ability.name;
+            return (
+              <div className="PokemonAbility" key={abilityName}>
+                {abilityName[0].toUpperCase() +
+                  abilityName.substring(1).replace("-", " ")}
+              </div>
+            );
+          })}
+        </div>
+        <h3>All known Moves</h3>
+        <div id="pokemon-movesets">
+          {/* Dynamically display vaild pokemon moves */}
+          {data.moves.map((move) => {
+            let moveName = move.move.name;
+            return (
+              <div className="PokemonMove" key={moveName}>
+                {moveName[0].toUpperCase() +
+                  moveName.substring(1).replace("-", " ")}
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  });
+};
+
+export default Results;
